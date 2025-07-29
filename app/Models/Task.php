5f3c45a9678a22461228b5aa049b0051610380d4
Danward Bautista <danwarddeveloper@gmail.com>
2025-07-29 14:47:34 +0800

    Laravel 10 - Initial commit
    -Task migration with logic
    -Task model

---
+++ app/Models/Task.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Task extends Model
+{
+    protected $fillable = [
+        'title',
+        'description',
+        'status',
+        'is_sub_task',
+        'parent_task_id',
+        'user_id', // remember to remove null later on auth
+        'deleted_at',
+        'permanent_delete_at',
+    ];
+
+    protected $casts = [
+        'is_sub_task' => 'boolean',
+        'deleted_at' => 'datetime',
+        'permanent_delete_at' => 'datetime',
+    ];
+
+
+    // force later on auth
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+    
+    public function parentTask(): BelongsTo
+    {
+        return $this->belongsTo(Task::class, 'parent_task_id');
+    }
+
+    public function subTasks(): HasMany
+    {
+        return $this->hasMany(Task::class, 'parent_task_id');
+    }
+}
---
+++ database/migrations/2025_07_29_040339_create_tasks_table.php
@@ -0,0 +1,50 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+
+            // Tasks main columns
+            $table->id();
+            $table->string('title');
+            $table->text('description')->nullable();
+            $table->enum('status', ['to-do', 'in-progress', 'done'])->default('to-do');
+            $table->timestamps();
+
+            // User reference stuff
+            $table->unsignedBigInteger('user_id')->nullable(); //remove null later on auth
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('set null');
+
+            // Sub tasks columns 
+            $table->boolean('is_sub_task')->default(false);
+            $table->unsignedBigInteger('parent_task_id')->nullable();
+
+            // Custom trash columns instead of soft deletes 
+            $table->timestamp('deleted_at')->nullable();
+            $table->timestamp('permanent_delete_at')->nullable();
+
+            // Indexing for search
+            $table->index('title');
+            $table->index(['parent_task_id', 'is_sub_task']);
+            $table->index('deleted_at');
+            $table->index('permanent_delete_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tasks');
+    }
+};
